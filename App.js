b56697a95747ee6cb7687d98955c3a11aaef9177
SR7 <admin@sr7.tech>
2019-02-04 15:57:41 +0500

    Native Base With Expo

--- App.js
+++ App.js
@@ -1,23 +1,8 @@
-import React from 'react';
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import reducers from "./src/Store/Reducers";
+import React from "react";
+import Setup from './src/boot/Setup';
 
-import Test from "./src/Components/Test";
-
-
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
-    
-    const store = createStore(combineReducers(reducers), compose(applyMiddleware(thunk)));
-
-    return (
-      <Provider store={store}>
-       <Test/>
-      </Provider>
-    );
+    return <Setup />;
   }
 }
-
-export default App
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Test from "./Components/Test";
+
+class App extends React.Component {
+  render() {
+    return <Test/>;
+  }
+}
+
+export default App
